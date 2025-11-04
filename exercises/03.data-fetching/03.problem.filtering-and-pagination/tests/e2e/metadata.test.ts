65e006948dd1b05c97ed1b5f9f765d921be1bb7b
Alem Tuzlak <t.zlak@hotmail.com>
2025-11-04 10:16:53 +0100

    fix up tests

--- exercises/03.data-fetching/03.problem.filtering-and-pagination/tests/e2e/metadata.test.ts
+++ exercises/03.data-fetching/03.problem.filtering-and-pagination/tests/e2e/metadata.test.ts
@@ -8,27 +8,7 @@ test.describe('Routing E2E Tests', () => {
 		await expect(page).toHaveTitle('Epic Shop')
 
 		// Check for duplicate <title> elements
-		const titleElements = page.locator('title')
-		await expect(titleElements).toHaveCount(1)
-	})
-
-	test('should navigate to about page and render correct content', async ({
-		page,
-	}) => {
-		await page.goto('/about')
-		await expect(page).toHaveTitle('Epic Shop | About Us')
-		// Check for duplicate <title> elements
-		const titleElements = page.locator('title')
-		await expect(titleElements).toHaveCount(1)
-	})
-
-	test('should navigate to contact page and render correct content', async ({
-		page,
-	}) => {
-		await page.goto('/contact')
-		await expect(page).toHaveTitle('Epic Shop | Contact Us')
-		// Check for duplicate <title> elements
-		const titleElements = page.locator('title')
+		const titleElements = page.locator('head > title')
 		await expect(titleElements).toHaveCount(1)
 	})
 
@@ -38,27 +18,7 @@ test.describe('Routing E2E Tests', () => {
 		await page.goto('/products')
 		await expect(page).toHaveTitle('Epic Shop | All Products')
 		// Check for duplicate <title> elements
-		const titleElements = page.locator('title')
-		await expect(titleElements).toHaveCount(1)
-	})
-
-	test('should navigate to terms-of-service page and render correct content', async ({
-		page,
-	}) => {
-		await page.goto('/terms-of-service')
-		await expect(page).toHaveTitle('Epic Shop | Terms of Service')
-		// Check for duplicate <title> elements
-		const titleElements = page.locator('title')
-		await expect(titleElements).toHaveCount(1)
-	})
-
-	test('should navigate to terms-of-use page and render correct content', async ({
-		page,
-	}) => {
-		await page.goto('/terms-of-use')
-		await expect(page).toHaveTitle('Epic Shop | Terms of Use')
-		// Check for duplicate <title> elements
-		const titleElements = page.locator('title')
+		const titleElements = page.locator('head > title')
 		await expect(titleElements).toHaveCount(1)
 	})
 
@@ -68,7 +28,7 @@ test.describe('Routing E2E Tests', () => {
 		await page.goto('/products/1')
 		await expect(page).toHaveTitle('Epic Shop | Product overview')
 		// Check for duplicate <title> elements
-		const titleElements = page.locator('title')
+		const titleElements = page.locator('head > title')
 		await expect(titleElements).toHaveCount(1)
 	})
 })
